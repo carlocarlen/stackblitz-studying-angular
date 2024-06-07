1b141cf10735a93d62f80c6b27beeeffcd39427b
carlocarlen <carlocarlen@gmail.com>
2024-06-07 16:23:42 +0200

    decorators

--- src/query-children.component.ts
+++ src/query-children.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { AfterViewInit, Component, ViewChild } from "@angular/core";
 import { ListComponent } from "./list.component";
 
 @Component({
@@ -8,8 +8,16 @@ import { ListComponent } from "./list.component";
     template: `
       <h2>query children works!</h2>
       <app-list/>
+      <p>Got {{ listlength }} items in list</p>
+      <p>This is to compare the decorator-based queries with the signal based approach</p>
     `,
 })
-export class QueryChildrenComponent {
-    
+export class QueryChildrenComponent implements AfterViewInit {
+    @ViewChild(ListComponent) listChild!: ListComponent;
+
+    listlength: number = 0;
+
+    ngAfterViewInit(): void {
+        this.listlength = this.listChild.items.length;
+    }
 }
